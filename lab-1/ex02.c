920a76a653abc70b65854dc7ed6339855bc13c44
EvanGeorgie <godly4829@gmail.com>
2026-07-10 09:01:12 +0000

    Complete Lab

---
+++ lab-1/ex02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("%s %s","My name is ","Thanapat R.\n");
+    printf("%s %d %s","I am ",17," years old.\n");
+    printf("%s %.1f %s","I am ",180.0," cm tall\n");
+
+    return 0;
+}
